809f6edecd8772cc931b8cabdd263314b448ceed
leduykhanhhedspi <leduykhanh.hedspi@gmail.com>
2016-05-23 10:55:24 +0700

    Add title function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def full_title(pagetitle='')
+		basetitle = "Ruby on Rails Tutorial Sample App"
+		if pagetitle.empty?
+			basetitle
+		else
+			pagetitle + " | " + basetitle
+		end
+		
+	end
 end
