215316180f863323543a0743594f530580413494
leduykhanhhedspi <leduykhanh.hedspi@gmail.com>
2016-06-03 17:25:43 +0700

    add accout activation
    fix
    
    fix chapter10
    
    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-          log_in user
-          flash[:success] = "Login success"
-          params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-          redirect_to user		 		
-  	else
-  	      flash.now[:danger] = 'Invalid Input'
-  	      render 'new'
-  	end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ User.create!(name:  "Example User",
              phonenumber: "0982432230",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -21,5 +23,7 @@ User.create!(name:  "Example User",
                email: email,
                phonenumber: phonenumber,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
